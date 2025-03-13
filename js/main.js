7a6d50be43b6394faa60c260cd4fff2851be58b0
Renren <johnlawrencemartinez05@gmail.com>
2025-03-13 17:57:14 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Monthsary Babeh!').split('') 
+    const titles = ('"Happy Monthsary Babeh! As the days turn into weeks and the weeks into months, our love continues to grow stronger."').split('') 
     const titleElement = document.getElementById('title');
     let index = 0;
 
