b1c5fafa3f6d6cb44c2286d850e8f535c4fba8a4
Renren <johnlawrencemartinez05@gmail.com>
2025-03-14 04:07:50 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('"Happy Monthsary Babeh! Our love continues to grow stronger."').split('') 
+    const titles = ('Happy Monthsary Babeh!').split('') 
     const titleElement = document.getElementById('title');
     let index = 0;
 
